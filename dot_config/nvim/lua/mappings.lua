e11cec81bd31424ad77178f78d112e66614ddcb1
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2024-03-20 20:10:18 +0100

    fist commit

---
+++ dot_config/nvim/lua/mappings.lua
@@ -0,0 +1,22 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("i", "jk", "<ESC>")
+
+-- Map 'y' to yank
+map('v', 'y', 'y')
+
+-- Map 'p' to paste
+map('v', 'p', 'p')
+
+-- Map leader + 'y' to yank to clipboard
+map('v', '<Leader>y', '"+y')
+
+-- Map leader + 'p' to paste from clipboard
+map('v', '<C>p', '"+p')
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
