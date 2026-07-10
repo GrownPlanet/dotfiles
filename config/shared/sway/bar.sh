318ad90da44bdb18cde574709748a3fbd7d38f51
timo dh <timo.dehandschutter@gmail.com>
2026-07-10 18:08:45 +0200

    a little more configing

--- config/shared/sway/bar.sh
+++ config/shared/sway/bar.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 while true; do 
-  date=$(date +"%d-%m-%Y")
+  date=$(date +"%d-%m")
   time=$(date +"%H:%M:%S")
-  volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%d%%\n", $2 * 100}')
+  volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%d\n", $2 * 100}')
+  battery=$(cat /sys/class/power_supply/BAT1/capacity)
 
-  echo "$volume | $time | $date"
+  echo "$battery% | $volume% | $time | $date "
 
   sleep 1
 done
